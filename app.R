a68084e0a44f48fc4a00fd82b8767a19daa50e7d
EeethB <ethanbrockmann@gmail.com>
2022-07-13 14:57:49 -0500

    Very, very basic UI functionality

--- app.R
+++ app.R
@@ -1,41 +1,3 @@
-# #
-# # This is a Shiny web application. You can run the application by clicking
-# # the 'Run App' button above.
-# #
-# # Find out more about building applications with Shiny here:
-# #
-# #    http://shiny.rstudio.com/
-# #
-# 
-# library(shiny)
-# library(DT)
-# library(tidyverse)
-# library(shinydashboard)
-# 
-# # Define UI for application that draws a histogram
-# ui <- fluidPage(
-#     DT::dataTableOutput("flower")
-# )
-# 
-# # Define server logic required to draw a histogram
-# server <- function(input, output) {
-#     
-#     output$flower <- renderDataTable({
-#         dat <- tibble::tibble(file = dir("www")) %>% 
-#           mutate(image = str_glue('<img src="{file}" height = "25"></img>')) %>%
-#           pivot_wider(names_from = file, values_from = image)
-#         
-#         datatable(dat, escape = F)
-#         }
-#     )
-# }
-# 
-# # Run the application 
-# shinyApp(ui = ui, server = server)
-
-
-
-
 library(shiny)
 library(tidyverse)
 library(gt)
@@ -48,8 +10,8 @@ library(gt)
 # uses the `gt_tbl` object as the input
 # expression
 
-gt_tbl <- read_csv("mems.csv") %>% 
-  gt() %>% 
+gt_tbl <- read_csv("mems.csv") %>%
+  gt() %>%
   text_transform(
     locations = cells_body(columns = c("pic", "snip")),
     fn = function(x) {
@@ -60,26 +22,41 @@ gt_tbl <- read_csv("mems.csv") %>%
 
 ui <- fluidPage(
   shinyjs::useShinyjs(),
-  gt_output(outputId = "friends"),
-  imageOutput("pic")
+  uiOutput("pic_path_out"),
+  fluidRow(imageOutput("pic"), style = "height:510px; text-align: center;"),
+  fluidRow(gt_output(outputId = "friends"))
+  
 )
 
 server <- function(input,
                    output,
                    session) {
-    
+  output$pic_path_out <- renderUI({
+    ui_pic_path <- textInput("pic_path", "", value = "ali-rose.jpg")
+    shinyjs::hidden(ui_pic_path)
+  })
+  
   output$friends <-
-    render_gt(
-      expr = gt_tbl,
-      height = px(600),
-      width = px(600)
-    )
+    render_gt(expr = gt_tbl,
+              height = px(600),
+              width = px(600))
   
   output$pic <-
-    renderImage({list(src = "./www/ali-rose.jpg", width = "50px", height = "50px", contentType = "image/jpg")}, deleteFile = FALSE)
+    renderImage({
+      pic_path_value <-
+        if_else(is.null(input$pic_path), "ali-rose.jpg", input$pic_path)
+      list(
+        src = str_glue("./www/{pic_path_value}"),
+        height = "500px",
+        contentType = "image/jpg"
+      )
+    }, deleteFile = FALSE)
   
+  # Walk the gt table columns pic and snip and make an onclick method
   walk(c(gt_tbl$`_data`$pic, gt_tbl$`_data`$snip),
-       ~shinyjs::onclick(.x, shinyjs::info(date())))
+       ~ shinyjs::onclick(.x, {
+         updateTextInput(inputId = "pic_path", value = .x)
+       }))
 }
 
 
