8e3e129ba741dfdd64fcf1ec340c0c7713432fc6
EeethB <ethanbrockmann@gmail.com>
2022-08-05 15:44:37 -0500

    Actually reads in from a stories table now! And it can handle multiple stories per user

--- app2.R
+++ app2.R
@@ -4,108 +4,129 @@ source("utils.R")
 source("read_data.R")
 
 ui <- miniUI::miniPage(
-  
   shinyjs::useShinyjs(),
   
   miniUI::miniTitleBar(
-    
     title = shiny::img(src = "rpals_logo.png", height = "30px"),
     
     right = shinyauthr::logoutUI(id = "logout")
     
-  ),
-  
+  ), 
+
   miniUI::miniTabstripPanel(
-    
     miniUI::miniTabPanel(
-      
       title = "Log In",
-      
+
       icon = shiny::icon("fas fa-sign-in-alt"),
-      
-      miniUI::miniContentPanel(
-        
-        shinyauthr::loginUI(id = "login")
-      
-      )
-      
+
+      miniUI::miniContentPanel(shinyauthr::loginUI(id = "login"))
+
     ),
-    
+
     miniUI::miniTabPanel(
-      
       title = "Feed",
-      
+
       icon = shiny::icon("table"),
-      
+
       miniUI::miniContentPanel(
-        
-        gt::gt_output("pals"),
-        
-        shiny::actionButton("reload_pals", "Refresh",
-                          icon = shiny::icon("fas fa-refresh")),
-        
-        shiny::actionButton("story-current", "Story from current script",
-                          icon = shiny::icon("far fa-file-code")),
-        
-        shiny::actionButton("story-clip", "Story from Clipboard",
-                          icon = shiny::icon("far fa-clipboard-list")),
-        
+        gt::gt_output("stories"),
+
+        shiny::actionButton(
+          "refresh_stories",
+          "Refresh",
+          icon = shiny::icon("fas fa-refresh")
+        ),
+
+        shiny::actionButton(
+          "story-current",
+          "Story from current script",
+          icon = shiny::icon("far fa-file-code")
+        ),
+
+        shiny::actionButton(
+          "story-clip",
+          "Story from Clipboard",
+          icon = shiny::icon("far fa-clipboard-list")
+        ),
+
         # shiny::fileInput("story-file")
-        
+
       )
-    
-    ), 
-    
+
+    ),
+
     miniUI::miniTabPanel(
-      
       title = "Story",
-      
+
       icon = shiny::icon("far fa-file-code"),
-      
-      miniUI::miniContentPanel(
-                   
-      )
-      
+
+      miniUI::miniContentPanel()
+
     )
-    
+
   )
   
 )
 
 server <- function(input, output, session) {
-  
   # Auth ----------------------------------------------------------------------
   credentials <- shinyauthr::loginServer(
     id = "login",
     data = df_creds,
     user_col = user,
     pwd_col = password,
-    log_out = reactive(logout_init())
+    log_out = shiny::reactive(logout_init())
   )
   
   logout_init <- shinyauthr::logoutServer(
     id = "logout",
-    active = reactive(credentials()$user_auth)
+    active = shiny::reactive(credentials()$user_auth)
   )
   
   # Other ---------------------------------------------------------------------
-  df_pals <- eventReactive(input$reload_pals, {
-    
+  df_pals <- shiny::eventReactive(input$reload_pals, {
     read_pals() %>%
       dplyr::mutate(button = purrr::map(
         uname,
         .f = ~ action_button_gt(
           .x,
-          "_button",
-          label = fontawesome::fa("far fa-file-code", stroke = "grey", height = "30px")
+          "_story",
+          label = fontawesome::fa(
+            "far fa-file-code",
+            stroke = "grey",
+            height = "30px"
+          )
         )
       ))
     
   })
   
+  stories_index <- 1
+  
+  df_stories <- shiny::eventReactive(input$refresh_stories, {
+    read_stories(stories_index) %>%
+      dplyr::left_join(read_pals(), by = "uname") %>% 
+      dplyr::mutate(button = purrr::map2(
+        .x = uname,
+        .y = stringr::str_replace_all(story_time, "[[:punct:]|[:space:]]", "_"),
+        .f = ~ action_button_gt(
+          .x,
+          "_story_",
+          .y,
+          label = fontawesome::fa(
+            "far fa-file-code",
+            stroke = "grey",
+            height = "30px"
+          )
+        )
+      ))
+    
+  })
+  
+  # df_stories <- reactive(read_stories())
+  
   # Render pals gt, including subbing in PFPs ---------------------------------
   output$pals <- gt::render_gt({
-    
     gt::gt(df_pals()) %>%
       gt::text_transform(
         locations = gt::cells_body(columns = c("pic")),
@@ -116,14 +137,37 @@ server <- function(input, output, session) {
     
   })
   
+  # Render pals gt, including subbing in PFPs ---------------------------------
+  output$stories <- gt::render_gt({
+    gt::gt(df_stories()) %>%
+      gt::text_transform(
+        locations = gt::cells_body(columns = c("pic")),
+        fn = function(x) {
+          paste0("<img id=", x, " src=", x, " height=30px></img>")
+        }
+      ) %>% 
+      gt::cols_hide(c(story, story_time)) %>% 
+      gt::cols_move(c(uname, name, button), after = pic)
+    
+  })
+  
   # Apply an onclick method to the `button` column in the pals gt -------------
   shiny::observe({
-    purrr::walk(
-      df_pals()$uname,
-      ~ shinyjs::onclick(paste0(.x, "_button"), {
-        open_user_file(paste0("# ", .x, "'s code"), .x, tempdir())
-      })
-    )
+    purrr::walk(df_pals()$uname,
+                ~ shinyjs::onclick(paste0(.x, "_story"), {
+                  read_user_story(.x, .y)
+                  open_user_file(paste0("# ", .x, "'s code"), .x, tempdir())
+                }))
+    
+  })
+  
+  shiny::observe({
+    purrr::walk2(df_stories()$uname, stringr::str_replace_all(df_stories()$story_time, "[[:punct:]|[:space:]]", "_"),
+                ~ shinyjs::onclick(paste0(.x, "_story_", .y), {
+                  story <- read_user_story(.x, .y)
+                  open_user_file(story, .x, tempdir())
+                }))
+
   })
   
 }
--- utils.R
+++ utils.R
@@ -1,7 +1,13 @@
-action_button_gt <- function(value, inputid, ...) {
-  as.character(shiny::actionLink(paste0(value, inputid),
+action_button_gt <- function(value, inputid, ts, ...) {
+  as.character(shiny::actionLink(paste0(value, inputid, ts),
                                    ...)) %>%
     gt::html()
+}
+
+read_user_story <- function(uname, ts) {
+  readr::read_csv("stories.csv") %>% 
+    dplyr::filter(uname == uname, story_time == lubridate::ymd_hms(ts)) %>% 
+    purrr::pluck("story")
 }
 
 open_user_file <- function(text, user, session_temp_dir) {
@@ -15,3 +21,14 @@ open_user_file <- function(text, user, session_temp_dir) {
 read_pals <- function() {
   readr::read_csv("pals.csv")
 }
+
+read_stories <- function(story_index) {
+  readr::read_csv("stories.csv") %>%
+    dplyr::arrange(desc(story_time)) %>% 
+    dplyr::slice((story_index * 5 - 4):(story_index * 5))
+}
+
+read_creds <- function() {
+  readr::read_csv("creds.csv")
+}
+
